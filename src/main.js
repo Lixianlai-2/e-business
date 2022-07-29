85fe7d9ae464fee722f273226f1ea777185c828b
Li <1643565650@qq.com>
2022-07-29 16:11:28 +0800

    用计时器延迟实现轮播图的功能

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import TypeNav from "./components/TypeNav";
 import store from "./store";
 // 在入口文件引入，就相当于把mockServe的代码在这里执行了一次
 import "@/mock/mockServe";
-
+// 引入swiper的css样式，swiper存在于node_modules里面
+import "swiper/css/swiper.css";
 // 注意，第一个参数用字符串，或者直接用TypeNav，都是会报错的
 Vue.component(TypeNav.name, TypeNav);
 
